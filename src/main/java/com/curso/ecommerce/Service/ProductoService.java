64395523e1ad17f9d858b8966a95316b12eb8308
raq-dev <74838368+raq-dev@users.noreply.github.com>
2022-05-21 12:26:19 -0400

    crear repository y las clases y el metodo show de productoController

---
+++ src/main/java/com/curso/ecommerce/Service/ProductoService.java
@@ -0,0 +1,15 @@
+package com.curso.ecommerce.Service;
+
+import java.util.Optional;
+
+import com.curso.ecommerce.model.Producto;
+
+public interface ProductoService {
+	public Producto save(Producto producto);
+	
+	public Optional<Producto> get(Integer id);
+	
+	public void update(Producto producto);
+	
+	public void delete(Integer id);	
+}
